620ca7fa162124c1f03df3664bff391241fc3ae5
caf-3 <tomas.caetano@ubi.co.mz>
2021-09-25 07:39:55 +0200

    origin renamed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           By there!!!
-          File mapping
         </p>
         <a
           className="App-link"
